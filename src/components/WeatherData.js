a600ebebe931b6826e536b70a135778957c81af9
Derian Pinto <pintoderian@gmail.com>
2018-06-27 00:37:46 -0500

    Añadiendo iconos

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -4,7 +4,7 @@ import WeatherExtraInfo from './WeatherExtraInfo';
 
 const WeatherData = () => (
     <div>
-        <WeatherTemperature></WeatherTemperature>
+        <WeatherTemperature temperature={20} weatherState={'cloud'}></WeatherTemperature>
         <WeatherExtraInfo humidity={80} wind={'10m/s'}></WeatherExtraInfo>
     </div>
 );
--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
 
-const WeatherTemperature = () => (
-    <div><span>12 C°</span></div>
+const getWeatherIcon = (weatherState) => {
+    switch (weatherState) {
+        case "cloud":
+            return (<WeatherIcons name="cloud" size="2x" />);    
+        default:
+            return (<WeatherIcons name="sleet" size="2x" /> );
+    }
+};
+
+const WeatherTemperature = ({temperature, weatherState}) => (
+    <div>
+        {getWeatherIcon(weatherState)}       
+        <span>{` ${temperature} C°`}</span>
+    </div>
 );
 
 export default WeatherTemperature;
